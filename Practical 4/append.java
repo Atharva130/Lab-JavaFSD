822b015a1e5ab066d1e8ed79dba687c11b946956
Atharva Adhar Rahate <rahateatharva13@gmail.com>
2025-12-01 14:58:45 +0530

    Create append.java

---
+++ Practical 4/append.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Pr1_AppendToFile {
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("example.txt", true);
+            writer.write("\nThis is the appended text.");
+            writer.close();
+            System.out.println("Text appended successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+}
