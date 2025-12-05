9a2875eada8d1555ae702183690c79dab7f6f8ec
Atharva Adhar Rahate <rahateatharva13@gmail.com>
2025-12-05 18:21:16 +0530

    Remove unnecessary header from HibernateUtil.java

---
+++ MiniProject/src/main/java/com/student/attendance/HibernateUtil.java
@@ -0,0 +1,13 @@
+package com.student.attendance;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sf = new Configuration().configure().buildSessionFactory();
+
+    public static SessionFactory getSessionFactory() {
+        return sf;
+    }
+}
